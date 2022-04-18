8d80eceb91179e794d695e950882f06642669561
William Yu <88666309+wyu6609@users.noreply.github.com>
2022-04-18 17:24:05 -0400

    updated, added voice to text

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -6,49 +6,96 @@ import AttachFileIcon from "@mui/icons-material/AttachFile";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
 import InsertEmoticonIcon from "@mui/icons-material/InsertEmoticon";
 import MicIcon from "@mui/icons-material/Mic";
-import ChatWebSocket from './ChatWebSocket'
-import StatusWebSocket from './StatusWebSocket'
-import Message from './Message'
-import { useSelector } from 'react-redux'
-import UserMenu from './UserMenu'
-const Chat = ({cableApp}) => {
+import ChatWebSocket from "./ChatWebSocket";
+import StatusWebSocket from "./StatusWebSocket";
+import Message from "./Message";
+import { useSelector } from "react-redux";
+import UserMenu from "./UserMenu";
+
+const SpeechRecognition =
+  window.SpeechRecognition || window.webkitSpeechRecognition;
+const mic = new SpeechRecognition();
+mic.continuous = true;
+mic.interimResults = true;
+mic.lang = "en-US";
+
+const Chat = ({ cableApp }) => {
+  const [messagePlaceHolder, setMessagePlaceHolder] =
+    useState("Type a message...");
+  const [isListening, setIsListening] = useState(false);
+
   const [seed, setSeed] = useState("");
   const [newMessage, setNewMessage] = useState("");
-  const currentRoom = useSelector((state)=> state.room.value)
-  const currentUser = useSelector((state)=> state.user.value)
+  const currentRoom = useSelector((state) => state.room.value);
+  const currentUser = useSelector((state) => state.user.value);
   const [userMenu, setUserMenu] = useState(false);
+
   useEffect(() => {
     setSeed(Math.floor(Math.random() * 5000));
   }, []);
+
   const handleChange = (e) => {
     setNewMessage(e.target.value);
   };
+
+  useEffect(() => {
+    handleListen();
+  }, [isListening]);
+
+  const handleListen = () => {
+    if (isListening) {
+      mic.start();
+      mic.onend = () => {
+        console.log("continue..");
+        mic.start();
+      };
+    } else {
+      mic.stop();
+      mic.onend = () => {
+        console.log("Stopped Mic on Click");
+      };
+    }
+    mic.onstart = () => {
+      console.log("Mics on");
+    };
+
+    mic.onresult = (event) => {
+      const transcript = Array.from(event.results)
+        .map((result) => result[0])
+        .map((result) => result.transcript)
+        .join("");
+      console.log(transcript);
+      setNewMessage(transcript);
+      mic.onerror = (event) => {
+        console.log(event.error);
+      };
+    };
+  };
+
   const handleSubmit = (e) => {
     e.preventDefault();
     const message = {
       content: newMessage,
       user_id: currentUser.id,
-      room_id: parseInt(currentRoom.room.id)
-  }
-  console.log("message",message);
-  fetch("http://localhost:3000/messages", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-                Accept: "application/json"
-            },
-            body: JSON.stringify({message: message})
-        })
-        .then(resp => resp.json())
-        .then(result => {
-            
-        })
-  }
-  function handelShowUsers(){
-    if(userMenu == false){
-      setUserMenu(true)
-    }else{
-      setUserMenu(false)
+      room_id: parseInt(currentRoom.room.id),
+    };
+    console.log("message", message);
+    fetch("http://localhost:3000/messages", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify({ message: message }),
+    })
+      .then((resp) => resp.json())
+      .then((result) => {});
+  };
+  function handelShowUsers() {
+    if (userMenu == false) {
+      setUserMenu(true);
+    } else {
+      setUserMenu(false);
     }
   }
   return (
@@ -66,12 +113,12 @@ const Chat = ({cableApp}) => {
           <IconButton>
             <AttachFileIcon />
           </IconButton>
-          <IconButton>
-            <MoreVertIcon onClick={handelShowUsers}/>
+          <IconButton onClick={handelShowUsers}>
+            <MoreVertIcon />
           </IconButton>
         </div>
       </div>
-      {userMenu ? <UserMenu/>: null}
+      {userMenu ? <UserMenu /> : null}
       <div className="chat-body">
         {currentRoom.messages.map((m) => {
           if (m.user_id == currentUser.id) {
@@ -89,19 +136,20 @@ const Chat = ({cableApp}) => {
 
         <form onSubmit={(e) => handleSubmit(e)}>
           <input
-            placeholder="Type a message..."
+            placeholder={isListening ? "Listening..." : "Type a message..."}
             type="text"
             value={newMessage}
             onChange={(e) => handleChange(e)}
           />
           <button>Send a message</button>
         </form>
-        <IconButton>
-          <MicIcon onClick={console.log("clicked")} />
+        <IconButton onClick={() => setIsListening((prevState) => !prevState)}>
+          <MicIcon />
         </IconButton>
-      <ChatWebSocket cableApp={cableApp}/>
-      {/* <StatusWebSocket cableApp={cableApp}/> */}
-    </div>
+
+        <ChatWebSocket cableApp={cableApp} />
+        {/* <StatusWebSocket cableApp={cableApp}/> */}
+      </div>
     </div>
   );
 };
